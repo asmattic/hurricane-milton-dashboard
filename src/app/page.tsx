03862c85824acccd696ea06414cc6334e8084203
Matt Oldfield <mattoldfieldweb@gmail.com>
2024-10-09 14:15:11 -0400

    fix: dynamic import HurricaneMap client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
-import HurricaneMap from "./components/HurricaneMap";
 
+import dynamic from 'next/dynamic'
+
+const HurricaneMapNoSSR = dynamic(
+  () => import('@/app/components/HurricaneMap'),
+  { ssr: false }
+)
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <HurricaneMap />
+        <HurricaneMapNoSSR />
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
